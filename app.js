ac838f4543b11f7e1640d9641e51672f423495bf
Antonio Mindov <amindov98@gmail.com>
2021-02-20 02:27:14 +0200

    Added basic interclient communication

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
--- socket-connection/index.js
+++ socket-connection/index.js
@@ -5,13 +5,38 @@ let debug = require('debug')('weblend-server:socket-connection');
 function initializeSockets(httpServer) {
     io = io(httpServer, {
         cors: {
-            origin: '*', // TODO: Check if there's a better way for this
-            methods: ['GET', 'POST']
+            origin: '*',
         }
     });
 
+    const scene_sockets = {};
+
     io.on('connection', (socket) => {
-        debug("socket connection received");
+        debug('a user connected');
+        let scene_id = null;
+
+        socket.on('open scene', ({to_scene_id}) => {
+            scene_id = to_scene_id;
+            if (!scene_sockets[scene_id]) {
+                scene_sockets[scene_id] = []
+            }
+            scene_sockets[scene_id].push(socket);
+            debug("added to scene")
+        });
+
+        socket.on('command', (command) => {
+            socket.broadcast.emit('command', command);
+        })
+
+        socket.on('disconnect', () => {
+            if (scene_id) {
+                const idx = scene_sockets[scene_id].indexOf(socket);
+                if (idx >= 0) {
+                    scene_sockets[scene_id].splice(idx);
+                    debug("removed from scene")
+                }
+            }
+        });
     })
 
 
