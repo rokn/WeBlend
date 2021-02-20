e9571f062bfef5526df9137eb5e4e253cb21e858
Antonio Mindov <amindov98@gmail.com>
2021-02-20 15:03:58 +0200

    Added usernames

--- socket-connection/index.js
+++ socket-connection/index.js
@@ -15,24 +15,39 @@ function initializeSockets(httpServer) {
         debug('a user connected');
         let scene_id = null;
 
-        socket.on('open scene', ({to_scene_id}) => {
+        socket.on('open scene', ({to_scene_id, username}) => {
             scene_id = to_scene_id;
             if (!scene_sockets[scene_id]) {
                 scene_sockets[scene_id] = []
             }
+
+            for (const sock of scene_sockets[scene_id]) {
+                sock.emit('open scene', {username: username});
+                socket.emit('open scene', {username: sock.username});
+            }
+
+            socket.username = username;
             scene_sockets[scene_id].push(socket);
-            debug("added to scene")
         });
 
         socket.on('command', (command) => {
-            socket.broadcast.emit('command', command);
+            if (!scene_id) return;
+            for (const sock of scene_sockets[scene_id]) {
+                if (sock === socket) continue;
+                sock.emit('command', command);
+            }
         })
 
         socket.on('disconnect', () => {
             if (scene_id) {
                 const idx = scene_sockets[scene_id].indexOf(socket);
                 if (idx >= 0) {
-                    scene_sockets[scene_id].splice(idx);
+                    scene_sockets[scene_id].splice(idx, 1);
+
+                    for (const sock of scene_sockets[scene_id]) {
+                        debug(`Notifying ${sock.username} that ${socket.username} left`)
+                        sock.emit('close scene', {username: socket.username});
+                    }
                     debug("removed from scene")
                 }
             }
